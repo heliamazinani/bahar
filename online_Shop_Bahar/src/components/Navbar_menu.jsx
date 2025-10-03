ce4a52c941a55f3d2b5ca24d44e8163db79ed7dc
HeliaMazinani <13helia1383@gmail.com>
2025-10-03 15:37:48 +0330

    nav

---
+++ online_Shop_Bahar/src/components/Navbar_menu.jsx
@@ -0,0 +1,45 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { Form,Button } from "react-bootstrap";
+
+function NavbarMenu() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">My Shop</Navbar.Brand>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#products">Products</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavbarMenu;
---
+++ online_Shop_Bahar/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import Navbar_menu from "../components/Navbar_menu";
+function Home(){
+    return <div>
+        <Navbar_menu/>
+    </div>;
+}
+export default Home;
