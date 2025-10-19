b8b28b7df601162dd57ab51cc2616c2a76d03bb0
HeliaMazinani <13helia1383@gmail.com>
2025-10-19 13:02:11 +0330

    admin panel layout

--- online_Shop_Bahar/src/App.jsx
+++ online_Shop_Bahar/src/App.jsx
@@ -6,63 +6,43 @@ import Footer from "./components/Footer";
 import Navbar_menu from "./components/Navbar_menu";
 import Products from "./pages/Products";
 import AuthPage from "./pages/AuthPage";
+import AdminPanel from "./pages/AdminPanel";
+import MainLayout from "./layouts/MainLayout";
+import AdminLayout from "./layouts/AdminLayout";
+
 function App() {
   return (
     <>
-      <div className="wave-container ">
-        <svg
-          viewBox="0 0 1440 320"
-          xmlns="http://www.w3.org/2000/svg"
-          className="animated-wave"
-        >
-          <defs>
-            <linearGradient id="waveGradient">
-              <stop offset="0%" stopColor="#ffd8ed">
-                <animate
-                  attributeName="stop-color"
-                  values="#ffd8ed; #f7a7e1; #ffdef6; #ffd8ed"
-                  dur="10s"
-                  repeatCount="indefinite"
-                />
-              </stop>
-              <stop offset="100%" stopColor="#f7a7e1">
-                <animate
-                  attributeName="stop-color"
-                  values="#f7a7e1; #ffdef6; #ffd8ed; #f7a7e1"
-                  dur="10s"
-                  repeatCount="indefinite"
-                />
-              </stop>
-            </linearGradient>
-          </defs>
-
-          <path
-            fill="url(#waveGradient)"
-            fillOpacity="1"
-            d=" M0,120 
-     C360,180 1080,60 1440,120 
-     L1440,0 
-     L0,0 
-     Z"
-          ></path>
-        </svg>
-      </div>
-
-      <div className="home px-lg-4 px-2">
-        {" "}
+ 
         <BrowserRouter>
-          {/* Navigation */}
-          <Navbar_menu />
-          {/* Routes */}
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/auth" element={<AuthPage />} />
+            <Route
+              path="/"
+              element={
+                <MainLayout>
+                  <Home />
+                </MainLayout>
+              }
+            />
+            <Route
+              path="/products"
+              element={
+                <MainLayout>
+                  <Products />
+                </MainLayout>
+              }
+            />
+            <Route
+              path="/admin"
+              element={
+                <AdminLayout>
+                  <AdminPanel />
+                </AdminLayout>
+              }
+            />
           </Routes>
         </BrowserRouter>
-      </div>
 
-      <Footer></Footer>
     </>
   );
 }
---
+++ online_Shop_Bahar/src/layouts/MainLayout.jsx
@@ -0,0 +1,54 @@
+import Footer from "../components/Footer";
+import Navbar_menu from "../components/Navbar_menu";
+
+export default function MainLayout({ children }) {
+  return (
+    <>
+      <div className="wave-container ">
+        <svg
+          viewBox="0 0 1440 320"
+          xmlns="http://www.w3.org/2000/svg"
+          className="animated-wave"
+        >
+          <defs>
+            <linearGradient id="waveGradient">
+              <stop offset="0%" stopColor="#ffd8ed">
+                <animate
+                  attributeName="stop-color"
+                  values="#ffd8ed; #f7a7e1; #ffdef6; #ffd8ed"
+                  dur="10s"
+                  repeatCount="indefinite"
+                />
+              </stop>
+              <stop offset="100%" stopColor="#f7a7e1">
+                <animate
+                  attributeName="stop-color"
+                  values="#f7a7e1; #ffdef6; #ffd8ed; #f7a7e1"
+                  dur="10s"
+                  repeatCount="indefinite"
+                />
+              </stop>
+            </linearGradient>
+          </defs>
+
+          <path
+            fill="url(#waveGradient)"
+            fillOpacity="1"
+            d=" M0,120 
+     C360,180 1080,60 1440,120 
+     L1440,0 
+     L0,0 
+     Z"
+          ></path>
+        </svg>
+      </div>
+
+      <div className="home px-lg-4 px-2">
+        <Navbar_menu />
+        <main>{children}</main>
+      </div>
+
+      <Footer></Footer>
+    </>
+  );
+}
